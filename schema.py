828a26a494638f3cb8f77131747580d729c539ab
asebn1 <asebn1@khu.ac.kr>
2020-08-21 10:25:08 +0900

    update

--- schema.py
+++ schema.py
@@ -1,17 +1,16 @@
-mport graphene
+import graphene
 
 class User(graphene.ObjectType):
-	hello = graphene.String(name=graphene.String(default_value="World"))
-	def resolve_hello(self, info, name):
-		return 'Hello ' + name
+    # Add something here
+    name = graphene.String(required=True)
+    description = graphene.String()
 
 class Query(graphene.ObjectType):
     user = graphene.Field(User, name=graphene.String())
 
     def resolve_user(self, info, name):
-        return Website(name="admin",
+        return User(name="admin",
                        description="I'm an admin I can do anything")
 
 schema = graphene.Schema(query=Query)
-result = schema.execute('{ hello }')
-print(result.data['hello']) # "Hello World"
+
