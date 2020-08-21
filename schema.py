9024b82ff996ba10515132c5fe79570e2298e277
kjaejoon114 <kjaejoon114@naver.com>
2020-08-21 10:23:16 +0900

    Schema

---
+++ schema.py
@@ -0,0 +1,13 @@
+import graphene
+
+class User(graphene.ObjectType):
+    # Add something here
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(User, name=graphene.String())
+
+    def resolve_user(self, info, name):
+        return Website(name="admin",
+                       description="I'm an admin I can do anything")
+
+schema = graphene.Schema(query=Query)
