a8e8e63b9850a059143e6ee4edf7512875a3d450
kjaejoon114 <kjaejoon114@naver.com>
2020-08-25 19:55:52 +0900

    modify

--- schema.py
+++ schema.py
@@ -1,13 +1,30 @@
 import graphene
 
+class SecretUser(graphene.ObjectType):
+    name = graphene.String(required=True)
+    description = graphene.String(deprecation_reason="I don't need this field anymore, it has a sensitive data")
+    secretfield = graphene.String()
+
 class User(graphene.ObjectType):
-    # Add something here
+    name = graphene.String(required=True)
 
 class Query(graphene.ObjectType):
     user = graphene.Field(User, name=graphene.String())
 
     def resolve_user(self, info, name):
-        return Website(name="admin",
+        return User(name="admin")
+
+class QuerySecret(graphene.ObjectType):
+    user = graphene.Field(SecretUser, name=graphene.String())
+
+    def resolve_user(self, info, name):
+        return SecretUser(name="admin",
                        description="I'm an admin I can do anything")
 
-schema = graphene.Schema(query=Query)
+IS_AUTHENTICATED = True
+
+if IS_AUTHENTICATED == True:
+    schema = graphene.Schema(query=QuerySecret)
+else:
+    schema = graphene.Schema(query=Query)
+
