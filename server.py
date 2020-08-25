42129286308dc1a60690abf026beb1c6d7229f66
kjaejoon114 <kjaejoon114@naver.com>
2020-08-25 19:54:37 +0900

    homework update

--- server.py
+++ server.py
@@ -1,18 +1,12 @@
 from flask import Flask
-
+from flask_graphql import GraphQLView
+from schema import schema
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def default_route():
-    return "Hello, Default Route!"
-
-@app.route('/index')
-def index():
-    return "Hello, World!"
-
-@app.route('/get_name')
-def get_name():
-    return "My name flask!"
+app.add_url_rule(
+ '/gql',
+ view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+)
 
 app.run()
 
