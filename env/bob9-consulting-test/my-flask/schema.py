ef1f972fc4f693521b7e5dfe9db8fd0c12379cc3
asebn1 <asebn1@khu.ac.kr>
2020-08-25 16:28:50 +0900

    modify

--- env/bob9-consulting-test/my-flask/schema.py
+++ env/bob9-consulting-test/my-flask/schema.py
@@ -1,16 +1,30 @@
 import graphene
 
-class User(graphene.ObjectType):
-    # Add something here
+class SecretUser(graphene.ObjectType):
     name = graphene.String(required=True)
     description = graphene.String(deprecation_reason="I don't need this field anymore, it has a sensitive data")
     secretfield = graphene.String()
+
+class User(graphene.ObjectType):
+    name = graphene.String(required=True)
+
 class Query(graphene.ObjectType):
     user = graphene.Field(User, name=graphene.String())
 
     def resolve_user(self, info, name):
-        return User(name="admin",
+        return User(name="admin")
+
+class QuerySecret(graphene.ObjectType):
+    user = graphene.Field(SecretUser, name=graphene.String())
+
+    def resolve_user(self, info, name):
+        return SecretUser(name="admin",
                        description="I'm an admin I can do anything")
 
-schema = graphene.Schema(query=Query)
+IS_AUTHENTICATED = True
+
+if IS_AUTHENTICATED == True:
+    schema = graphene.Schema(query=QuerySecret)
+else:
+    schema = graphene.Schema(query=Query)
 
